f991d5477bf30b2e3e664065cefd54944cf0fb3d
WandiaG <wandiagithinji@gmail.com>
2023-07-24 14:31:49 -0700

    Update

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,64 +6,41 @@
  *
  * Return: the int converted from the string
  */
-
 int _atoi(char *s)
 {
-        int i, d, n, len, f, digit;
-        i = 0;
-        d = 0;
-        n = 0;
-        f = 0;
+	int i, d, n, len, f, digit;
 
-        digit = 0;
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
 
+	while (s[len] != '\0')
+		len++;
 
-        while (s[len] != '\0')
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+			++d;
 
-                len++;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
+	}
 
+	if (f == 0)
+		return (0);
 
-        while (i < len && f == 0)
-
-        {
-
-                if (s[i] == '-')
-
-                        ++d;
-
-
-                if (s[i] >= '0' && s[i] <= '9')
-
-                {
-
-                        digit = s[i] - '0';
-
-                        if (d % 2)
-
-                                digit = -digit;
-
-                        n = n * 10 + digit;
-
-                        f = 1;
-
-                        if (s[i + 1] < '0' || s[i + 1] > '9')
-
-                                break;
-
-                        f = 0;
-
-                }
-
-                i++;
-
-        }
-
-
-        if (f == 0)
-
-                return (0);
-
-
-        return (n);
-
+	return (n);
 }
