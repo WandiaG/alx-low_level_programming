3acbf153b6d81c0e7478a7ac81419c07703fdd20
WandiaG <wandiagithinji@gmail.com>
2023-09-04 07:20:55 -0700

    reading the file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,19 +12,19 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buf;
-	ssize_t fptr;
+	ssize_t fd;
 	ssize_t w;
 	ssize_t t;
 
 	fd = open(filename, O_RDONLY);
-	if (fptr == -1)
+	if (fd == -1)
 		return (0);
 	buf = malloc(sizeof(char) * letters);
-	t = read(fptr, buf, letters);
+	t = read(fd, buf, letters);
 	w = write(STDOUT_FILENO, buf, t);
 
 	free(buf);
-	close(fptr);
+	close(fd);
 	return (w);
 }
 
