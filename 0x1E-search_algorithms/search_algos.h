51140dde0f54fdfab859428bb208a1c12d6fc787
WandiaG <wandiagithinji@gmail.com>
2024-02-07 02:10:11 -0800

    update

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef SEARCH_ALGO
+#define SEARCH_ALGO
+
+#include <stddef.h>
+#include <stdio.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+void print_array(int *array, size_t l, size_t r);
+
+#endif /* SEARCH_ALGO_ */
